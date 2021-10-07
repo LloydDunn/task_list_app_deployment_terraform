617946d50205ba18d51409b5c18149f4e48c7660
Simo Tchokni <simo@makers.tech>
2021-10-07 14:42:10 +0000

    Add aws/creae_dockerrun.sh script

---
+++ aws/create_dockerrun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# See link for format required by AWS for the Dockerrun.aws.json file
+# https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/single-container-docker-configuration.html
+cat <<EOF > Dockerrun.aws.json
+{
+  "AWSEBDockerrunVersion": "1",
+  "Image": {
+    "Name": "$CONTAINER_REGISTRY_URL/$CONTAINER_REPOSITORY_NAME:$CI_COMMIT_SHA",
+    "Update": "true"
+  },
+  "Ports": [
+    {
+      "ContainerPort": "80"
+    }
+  ]
+}
+EOF
+
+echo "Created Dockerrun file"
+cat Dockerrun.aws.json
