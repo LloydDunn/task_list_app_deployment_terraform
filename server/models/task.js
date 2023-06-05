648dea9d0d36d0602adb3d0e7c36199d36f3b274
Roi Driscoll <roi.driscoll@gmail.com>
2023-06-05 16:18:30 +0200

    Adds explicit declaration of created_at and updated_at casing

--- server/models/task.js
+++ server/models/task.js
@@ -6,6 +6,8 @@ module.exports = (sequelize, DataTypes) => {
       title: DataTypes.STRING,
       description: DataTypes.STRING,
       tech_stack: DataTypes.STRING,
+      created_at: DataTypes.DATE,
+      updated_at: DataTypes.DATE,
     },
     {}
   );
