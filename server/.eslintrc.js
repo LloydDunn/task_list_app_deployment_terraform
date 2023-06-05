853b0c66d72bc151039ef7db6e73127d7e70c0d9
Roi Driscoll <roi.driscoll@gmail.com>
2023-06-05 15:45:09 +0200

    Attempt to disable quote linting rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,19 +1,14 @@
 module.exports = {
-  extends: [
-    'airbnb-base',
-    'plugin:jest/recommended',
-  ],
-  plugins: [
-    'import',
-    'jest',
-  ],
+  extends: ["airbnb-base", "plugin:jest/recommended"],
+  plugins: ["import", "jest"],
   rules: {
     "import/no-dynamic-require": 0,
     "no-param-reassign": 0,
-    "no-useless-catch": 0
+    "no-useless-catch": 0,
+    quotes: "off",
   },
   env: {
     node: true,
-    'jest/globals': true,
+    "jest/globals": true,
   },
 };
